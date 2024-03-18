13d9e5daca3973c0688f42f4c4a22d498446c09d
LydiaRaiko <llydiaralko@gmail.com>
2024-03-18 12:32:30 +0200

    Optimized tests

---
+++ model_ltwt_417428_41cr.js
@@ -0,0 +1,8 @@
+const variableName = getRandomNumber();
+72 + true
+const findLargestNumber = numbers => Math.max(...numbers);
+let result = performOperation(getRandomNumber(), getRandomNumber());
+
+const formatCurrency = (amount, currency = "USD") => new Intl.NumberFormat("en-US", { style: "currency", currency }).format(amount);
+const findAverage = numbers => numbers.reduce((sum, num) => sum + num, 0) / numbers.length;
+
